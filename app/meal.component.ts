6c9231231f749f825cebd4f20a892d7834eeb824
Okwonks <albertoketch@gmail.com>
2017-07-19 19:30:03 +0300

    Fixing naming issues found in the if statement so as to ensure that the if statement runs as it should.

--- app/meal.component.ts
+++ app/meal.component.ts
@@ -5,8 +5,8 @@ import { Meal } from './meal.model';
   selector: 'meal-display',
   template: `
   <div>
-    <span *ngIf="meal.done === true" class="label label-danger" (click)="toggleDone(false)">{{meal.calories}}</span>
-    <span *ngIf="meal.done === false" class="label label-default" (click)="toggleDone(true)">{{meal.calories}}</span>
+    <span *ngIf="meal.calories === true" class="label label-danger" (click)="toggleDone(false)">{{meal.calories}}</span>
+    <span *ngIf="meal.calories === false" class="label label-default" (click)="toggleDone(true)">{{meal.calories}}</span>
   </div>
   `
 })
@@ -14,6 +14,6 @@ import { Meal } from './meal.model';
 export class MealComponent {
   @Input() meal: Meal;
   toggleDone(setCalories: boolean){
-    this.meal.done = setCalories;
+    this.meal.calorie = setCalories;
   }
 }
